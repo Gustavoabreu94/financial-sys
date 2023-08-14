ebfa9c884a13d0fea28dd04e8c35a2764a8889f5
Gustavo Alves Abreu <gustavoalvesabreu94@gmail.com>
2023-08-14 15:29:35 -0300

    create project and initials code

---
+++ src/data/items.ts
@@ -0,0 +1,5 @@
+export const items = [
+    { date: new Date(2022, 9, 15), category: 'food', title: 'McDonalds', value: 32.12 },
+    { date: new Date(2022, 9, 15), category: 'food', title: 'McDonalds', value: 32.12 },
+    { date: new Date(2022, 9, 15), category: 'food', title: 'McDonalds', value: 32.12 },
+]

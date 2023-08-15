576342bf769ff3e52a9b048599396b1da35d5c63
Gustavo Alves Abreu <gustavoalvesabreu94@gmail.com>
2023-08-15 16:07:15 -0300

    Implemented Summary and initializad InfoArea

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { useEffect, useState } from 'react';
 import * as C from './App.styles'
+import { Item } from './types/Item';
+import { Category } from './types/Category';
+import { items } from './data/items';
+import { categories } from './data/categories';
+import { getCurrentMonth, filterListByMonth } from './helpers/dataFilter';
+import { TableArea } from './components/TableArea'
+import { InfoArea } from './components/InfoArea';
 const App = () => {
+
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect(() => {
+    setFilteredList(filterListByMonth(list, currentMonth));
+  }, [list, currentMonth]);
+
   return (
     <C.Container>
       <C.Header>
@@ -8,7 +25,11 @@ const App = () => {
         </C.HeaderText>
       </C.Header>
       <C.Body>
-        ...
+        
+        <InfoArea currentMonth={currentMonth} />
+
+
+        <TableArea list={filteredList} />
       </C.Body>
     </C.Container>
   );
--- src/data/items.ts
+++ src/data/items.ts
@@ -1,5 +1,8 @@
-export const items = [
-    { date: new Date(2022, 9, 15), category: 'food', title: 'McDonalds', value: 32.12 },
-    { date: new Date(2022, 9, 15), category: 'food', title: 'McDonalds', value: 32.12 },
-    { date: new Date(2022, 9, 15), category: 'food', title: 'McDonalds', value: 32.12 },
-]
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+    { date: new Date(2023, 7, 15), category: 'food', title: 'McDonalds', value: 32.20 },
+    { date: new Date(2023, 7, 15), category: 'food', title: 'Burguer King', value: 28.00 },
+    { date: new Date(2023, 7, 16), category: 'salary', title: 'Aluguel Apt', value: 2200.00 },
+    { date: new Date(2022, 10, 18), category: 'salary', title: 'Salario', value: 1200.00 },
+];

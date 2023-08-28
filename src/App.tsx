6cbdfb37b0cb8386445705b2d10e16c2b8371895
Gustavo Alves Abreu <gustavoalvesabreu94@gmail.com>
2023-08-28 05:38:44 -0300

    Logic add itens

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { categories } from './data/categories';
 import { getCurrentMonth, filterListByMonth } from './helpers/dataFilter';
 import { TableArea } from './components/TableArea'
 import { InfoArea } from './components/InfoArea';
+import { InputArea } from './components/InputArea';
 const App = () => {
 
   const [list, setList] = useState(items);
@@ -38,6 +39,12 @@ const App = () => {
     setCurrentMonth(newMonth)
   }
 
+  const handleAddItem = (item: Item) => {
+    let newList = [...list];
+    newList.push(item);
+    setList(newList);
+  }
+
   return (
     <C.Container>
       <C.Header>
@@ -54,6 +61,7 @@ const App = () => {
           expense={expense}
         />
 
+        <InputArea onAdd={handleAddItem} />
 
         <TableArea list={filteredList} />
       </C.Body>
